4a8be64ffb53b3219503ad002af77a9335eb6a80
Michael Mendoza <engr.chaelmendoza@gmail.com>
2022-06-08 14:11:18 +0800

    feat: add update, delete and add user functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,80 @@
 import './App.css'
+import { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { addUser, deleteUser, updateUsername } from './features/Users'
 
 function App() {
+  const dispatch = useDispatch()
+  const userList = useSelector((state) => state.users.value)
+
+  const [name, setName] = useState('')
+  const [username, setUsername] = useState('')
+  const [newUsername, setNewUsername] = useState('')
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <div className="addUser">
+        <input
+          type="text"
+          placeholder="Name..."
+          onChange={(event) => {
+            setName(event.target.value)
+          }}
+        />
+        <input
+          type="text"
+          placeholder="Username"
+          onChange={(event) => {
+            setUsername(event.target.value)
+          }}
+        />
+        <button
+          onClick={() => {
+            dispatch(
+              addUser({
+                id: userList[userList.length - 1].id + 1,
+                name,
+                username,
+              })
+            )
+          }}
+        >
+          Add User
+        </button>
+      </div>
+      <div className="displayUsers">
+        {userList.map((user) => {
+          return (
+            <div>
+              <h1>{user.name}</h1> <h1>{user.username}</h1>
+              <section>
+                <input
+                  type="text"
+                  placeholder="Update username..."
+                  onChange={(event) => {
+                    setNewUsername(event.target.value)
+                  }}
+                />
+                <button
+                  onClick={() => {
+                    dispatch(
+                      updateUsername({ id: user.id, username: newUsername })
+                    )
+                  }}
+                >
+                  Update username
+                </button>
+                <button
+                  onClick={() => {
+                    dispatch(deleteUser({ id: user.id }))
+                  }}
+                >
+                  Delete username
+                </button>
+              </section>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { configureStore } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux'
+
+import usersReducer from './features/Users'
+
+const store = configureStore({
+  reducer: {
+    users: usersReducer,
+  },
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 )
